aa1696f4fcc9dfc1fa1f1936e866fb571de3f602
G4brielBarbosa <gabrielba@outlook.com.br>
2022-08-05 12:39:32 -0300

    First

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+$(document).ready(() =>{
+    var input = $("#email")
+    var button = $('.arrow');
+    
+
+    button.click(() => {
+
+        var user = input.val().substring(0, input.val().indexOf("@"))
+        var domain = input.val().substring(input.val().indexOf("@") + 1, input.val().length)
+
+        if((user.length >= 1) &&
+            (domain.length >= 3) &&
+            (user.search("@") == -1) &&
+            (domain.search("@") == -1)&&
+            (user.search(" ") == -1) &&
+            (domain.search(" ") == -1) &&
+            (domain.search(".") != -1) &&
+            (domain.indexOf(".") >= 1) &&
+            (domain.lastIndexOf(".") < domain.length -1)){
+                
+            }else{
+                console.log('erro')
+            }
+    })
+    
+    
+})
